2e4bdbae735c62ad24476bddf64337ea7b2820f8
shushant <shushantkumar0625@gmail.com>
2025-03-25 02:15:03 +0530

    change

--- src/app.js
+++ src/app.js
@@ -15,8 +15,10 @@ const PORT=process.env.PORT; //port number ko process.env.PORT me store krna hot
 app.use(express.json({ limit: '10mb' })); // parses incoming request withJSON payloads
 app.use(cookieParser()); // parses cookies attacged to the client request object
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,8 +6,8 @@ export const generateToken=(userId,res)=>{
  res.cookie("jwt",token,{
         httpOnly:true,
         maxAge:30*24*60*60*1000,
-        sameSite:"strict",
-        secure:process.env.NODE_ENV!=="development"
+        sameSite:"none",
+        secure: true
  })
  return token;
 
