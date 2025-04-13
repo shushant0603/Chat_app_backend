37c9ac9a27662d521a8d88a8b0103aa271438e1d
shushant <shushantkumar0625@gmail.com>
2025-04-13 19:44:44 +0530

    change

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,18 +1,15 @@
 import jwt from 'jsonwebtoken';
-// import dotenv from 'dotenv'; 
 export const generateToken=(userId,res)=>{
     const token=jwt.sign({id:userId},process.env.JWT_SECRET,{
         expiresIn:"30d",
     });
-//  res.cookie("jwt",token,{
-//         httpOnly:true,
-//         maxAge:30*24*60*60*1000,
-//         sameSite:"strict",
-//         secure:process.env.NODE_ENV==="production",
-//         // sameSite: "strict",
-//  })
-};
- export const verifyToken = (token) => {
-    return jwt.verify(token, process.env.JWT_SECRET);
-};
+ res.cookie("token",token,{
+        httpOnly:true,
+        maxAge:30*24*60*60*1000,
+        sameSite:"strict",
+        secure:process.env.NODE_ENV==="production",
+        sameSite: "strict",
+ })
+ return token;
 
+}
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,5 +1,4 @@
-// import jwt from "jsonwebtoken";
-import { verifyToken } from "../utils/token.js";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js"
 
 export const protectRoute=async(req,res,next)=>{
@@ -8,7 +7,7 @@ export const protectRoute=async(req,res,next)=>{
         if(!token){
             return res.status(401).json({message:"unauthorized: No token provided"});
         }
-        const decoded = verifyToken(token);
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
         if(!decoded || !decoded.userId){
             return res.status(401).json({message:"unauthorized: Invalid token"});    
         }
@@ -21,17 +20,7 @@ export const protectRoute=async(req,res,next)=>{
         next();
 
     }catch(err){
-        console.error("Error in protect route middleware:", err.message);
-
-        // Handle specific JWT errors
-        if (err.name === "TokenExpiredError") {
-            return res.status(401).json({ message: "Unauthorized: Token expired" });
-        }
-        if (err.name === "JsonWebTokenError") {
-            return res.status(401).json({ message: "Unauthorized: Invalid token" });
-        }
-
-        // Handle other errors
-        res.status(500).json({ message: "Internal server error" });
+        console.error("Error in protect route middleware",err.message);
+        res.status(500).json({message:"internal server error"})
     }
 }
