b23f28091fc9b4d220cb40e82723e90c12dd7bbe
shushant <shushantkumar0625@gmail.com>
2025-04-13 19:58:28 +0530

    change

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,4 +1,4 @@
-import { verifyToken } from "../utils/token.js";
+import { verifyToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
